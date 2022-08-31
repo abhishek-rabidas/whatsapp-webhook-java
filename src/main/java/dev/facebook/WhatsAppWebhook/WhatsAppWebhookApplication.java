723598d2b5133d914449fa90fea7fa18e1c51a43
abhishek <avisec07@gmail.com>
2022-08-31 23:31:59 +0530

    project initialization

---
+++ src/main/java/dev/facebook/WhatsAppWebhook/WhatsAppWebhookApplication.java
@@ -0,0 +1,13 @@
+package dev.facebook.WhatsAppWebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppWebhookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/facebook/WhatsAppWebhook/WhatsAppWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.facebook.WhatsAppWebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppWebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
